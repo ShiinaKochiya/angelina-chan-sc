7c9d234fba4056c31f8fa81e825043535f565eba
Sakura Kaslana <caovanviethung1412@gmail.com>
2021-11-26 19:08:07 +0700

    Updated a!info command

--- src/cmd/info.js
+++ src/cmd/info.js
@@ -6,7 +6,7 @@ module.exports = new Command({
 
     async run(message, args, client) {
         console.log(message.author.tag,"used a!info")
-        message.channel.send(`Developed by Sakura Kaslana#5414, code yeeted from Angelina-chan, another Sakura bot`)
+        message.channel.send(`Developed by Sakura Kaslana#5414, code yeeted from Angelina-chan, another Sakura bot\nSource code(Please read the README.md and LICENSE.ms before using this code): https://github.com/SakuraKaslana/angelina-chan-sc`)
 
     }
 });
