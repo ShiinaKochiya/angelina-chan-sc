1ba818105014d6131d65174e42353c5dfbb82197
Sakura Kaslana <caovanviethung1412@gmail.com>
2021-11-27 19:03:47 +0700

    Added seggs and wtf, which will send Takash SEGGGGGGSSSS and BTMC wtf

---
+++ src/cmd/seggs.js
@@ -0,0 +1,13 @@
+const Command = require("../structures/Command.js");
+
+module.exports = new Command({
+    name: "seggs",
+    description: "Takashi go SEGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG",
+
+    async run(message, args, client) {
+          message.channel.send({files: [{
+            attachment: './src/cmd/wtf/sex.mp4',
+            name: 'sex.mp4'
+          }]})
+    }   
+});
