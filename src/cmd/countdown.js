ea95a77e49923b0bddf2a1e8da2980ea063d3c35
Sakura Kaslana <caovanviethung1412@gmail.com>
2021-12-30 11:45:44 +0700

    fix countdown memory leak, update help command It got memory leaked smh, my bad

--- src/cmd/countdown.js
+++ src/cmd/countdown.js
@@ -8,17 +8,19 @@ module.exports = new Command({
         if (message.author.bot) return;
         var c = args.slice(1).join(" "); 
         var a = 0
+        var check = 1
         console.log(message.author.tag,"used a!countdown", c)
         if (message.author.bot) {
             message.reply(`Don't...`)
             return;
         } else if (c < 0) {message.reply('You entered an invalid number!')} else if (isNaN(c)){message.reply('You didnt provide a number at all!')} else if(c > 300){message.reply('You entered a number that is too large')} else {var a = 1}
         if (a = 1){
-        setInterval(function () {
+        let cd = setInterval(function () {
         c = c - 1;
+        message.channel.send(`${c}`)
         if (c === 0) {
-        message.reply("Timer Done!")
-        return;
+        message.reply("Timer Done!");
+        clearInterval(cd);
     }
 }, 1000)}
     }   
