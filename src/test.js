2638856ae6fd4c7ca74a701011506482b6d1b161
giabao06 <giabaodoremon@gmail.com>
2022-01-08 22:06:22 +0700

    Add test script

---
+++ src/test.js
@@ -0,0 +1,18 @@
+  /** @format */
+
+const Client = require("./structures/Client.js");
+const Command = require("./structures/Command.js");
+const fs = require("fs");
+
+fs.readdirSync("./src/cmd")
+	.filter(file => file.endsWith(".js"))
+	.forEach(file => {
+		/**
+		 * @type {Command}
+		 */
+		const command = require(`./cmd/${file}`);
+		console.log(`Command ${command.name} parse complete`);
+
+	});
+return 0;
+
