2fb3fc68ff03a5f331bbdbdbb681fd4e67c22939
giabao06 <giabaodoremon@gmail.com>
2022-01-13 15:52:21 +0700

    Add admin role ID in config.json and make shutdown use it

--- src/cmd/shutdown.js
+++ src/cmd/shutdown.js
@@ -1,4 +1,5 @@
 const Command = require("../structures/Command.js");
+const config = require("../data/config.json")
 
 module.exports = new Command({
     name: "shutdown",
@@ -7,13 +8,15 @@ module.exports = new Command({
     async run(message, args, client) {
         if (message.author.bot) return;
 
-        if (message.member.roles.cache.has(`834426705250746389`)){
+        if (message.member.roles.cache.has(config.adminRoles)){
             message.reply(`See ya next time`).then(() => {
              console.log(`Angie was shutted down using !shutdown`)   
              client.destroy();
+             return(0);
         })   
         } else {
             message.reply(`boi u're not a mod`)
         }
     }
+
 });
