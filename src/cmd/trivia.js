3d28ebad1567a44bd5322de92d7f0aebc80631e3
Sakura Kaslana <caovanviethung1412@gmail.com>
2022-02-06 22:40:14 +0700

    I forgor to change the command name :skull:

--- src/cmd/trivia.js
+++ src/cmd/trivia.js
@@ -2,15 +2,15 @@ const Command = require("../structures/Command.js");
 const quiz = require("../data/quiz.json");
 const { MessageFlags } = require("discord.js");
 module.exports = new Command({
-    name: "test",
-    description: "test",
+    name: "trivia",
+    description: "trivia",
 
     async run(message, args, client) {
         const item = quiz[Math.floor(Math.random() * quiz.length)];
         const filter = response => {
 	       return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
         };
-        
+
     message.reply(item.question, { fetchReply: true })
 	.then(() => {
 		message.channel.awaitMessages({ filter, max: 1, time: 15000, errors: ['time'] })
