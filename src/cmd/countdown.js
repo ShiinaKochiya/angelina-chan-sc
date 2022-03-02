257c8b1bd89d2d2666075c5f6a731b268c1aa82c
giabao06 <giabaodoremon@gmail.com>
2022-03-02 10:11:13 +0700

    Little adjustment to countdown log out

--- src/cmd/countdown.js
+++ src/cmd/countdown.js
@@ -8,13 +8,14 @@ module.exports = new Command({
     async run(message, args, client) {
         if (message.author.bot) return;
         var c = args.slice(1).join(" ");
+        if (c==='')return message.reply("Ya can't count in negative integers, dum");
         var a = 0
         var check = 1
         var time = new Date().toLocaleTimeString('en-US', { hour12: false,
                                                  hour: "numeric",
                                                  minute: "numeric",
                                                  second: "numeric"})
-        console.log("[",time,"]",message.author.tag,"used countdown", c)
+        console.log("[",time,"]",message.author.tag,"used countdown for", c,"seconds")
         if (message.author.bot) {
             message.reply(`Don't...`)
             return;
