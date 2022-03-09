7cbc0e7e3be142819a09baf163092de7991693c6
giabao06 <giabaodoremon@gmail.com>
2022-03-09 18:11:04 +0700

    Implementing an array of admin roles and test w/ shutdown

--- src/cmd/shutdown.js
+++ src/cmd/shutdown.js
@@ -8,11 +8,15 @@ module.exports = new Command({
 
     async run(message, args, client) {
         if (message.author.bot) return;
+        var check = new Boolean (false);
+        for (let i = 0; i < config.adminRoles; i++) {
+          if (message.member.roles.cache.has(config.adminRoles[i])) {check = true}
+}
         var time = new Date().toLocaleTimeString('en-US', { hour12: false,
                                                  hour: "numeric",
                                                  minute: "numeric",
                                                  second: "numeric"})
-        if (message.member.roles.cache.has(config.adminRoles)){
+        if (check == true){
             message.reply(`See ya next time`).then(() => {
              console.log("[",time,"]",`Shutdown requested by ${message.author.tag}, cya later`)
              client.destroy();
