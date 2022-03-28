5dcd8c411916dda4f3127bbe205410a10ee12d32
Sakura Kaslana <caovanviethung1412@gmail.com>
2022-03-28 19:34:59 +0700

    Previous commit pushed to the wrong branch :lmao: Fuck me

--- src/cmd/ship.js
+++ src/cmd/ship.js
@@ -2,11 +2,11 @@ const Command = require("../structures/Command.js");
 
 module.exports = new Command({
     name: "ship",
-    description: "Lets see how much likely you can marry the other person (btw this is rigged -giabao06)",
+    description: "Lets see how much likely you can marry the other person",
 
     async run(message, args, client) {
         if (message.author.bot) return;
-
+        
         var numba = Math.floor(Math.random() * 101);
     if (args.slice(2).join(" ")==="") {
         var name = message.author.username
@@ -17,9 +17,11 @@ module.exports = new Command({
         }
     var name2 = args.slice(1).join(" ");
     var te=0
-
+    
     var l2 = name2.length
     var name2 = name2.substr(0, l2-l)
+
+    console.log(message.author.tag,"used a!ship", name2, name)
     if (name2 === 'Angelina'){
         if (name === 'Sakura Kaslana'){
          message.reply(`${name} x ${name2}: 100 %`)
@@ -32,12 +34,17 @@ module.exports = new Command({
                     if (name === 'FreelanceCash53'){
                      message.reply(`${name} x ${name2}: 100 %`)
         } else {var te=1}}else {var te=1}
-    if (te===1) message.reply(`${name2} x ${name}: ${numba} %`)
-    var time = new Date().toLocaleTimeString('en-US', { hour12: false,
-                                             hour: "numeric",
-                                             minute: "numeric",
-                                             second: "numeric"})
-    console.log("[",time,"]",message.author.tag,`shipped ${name} with ${name2}`);
+    if (te===1) {
+        const s = args.slice(1).join(" ");
+        if (s.indexOf('-')>-1){
+            var check = true;
+        }
+        if (check === true){
+        var name = s.substring(0, s.indexOf(' -')); 
+        var name2 = s.substring(s.indexOf('- ')+2, s.length)
+        }
+        message.reply(`${name2} x ${name}: ${numba} %`)
+    }
 
     }
 });
