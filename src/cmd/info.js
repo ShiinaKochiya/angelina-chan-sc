a5fdf7ad3a01d393d0ead93b40c54bdf3a83bb75
SakuraKaslana <caovanviethung1412@gmail.com>
2022-07-12 23:19:18 +0700

    Update name in info

--- src/cmd/info.js
+++ src/cmd/info.js
@@ -11,7 +11,7 @@ module.exports = new Command({
                                                  minute: "numeric",
                                                  second: "numeric"})
         console.log("[",time,"]", message.author.tag,"used info")
-        message.channel.send(`Developed by Sakura Kaslana#5414, code yeeted from Angelina-chan, another Sakura bot\nSource code(Please read the README.md and LICENSE.ms before using this code): https://github.com/SakuraKaslana/angelina-chan-sc`)
+        message.channel.send(`Developed by Sakana Kochiya,Giabao06 and Seyun Sky, code yeeted from Angelina-chan, another Sakura bot\nSource code(Please read the README.md and LICENSE.ms before using this code): https://github.com/SakuraKaslana/angelina-chan-sc`)
 
     }
 });
