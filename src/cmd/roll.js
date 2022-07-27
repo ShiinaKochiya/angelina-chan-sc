68a3212f54e84c38bfc4bd1177df78285692d1ee
SakuraKaslana <caovanviethung1412@gmail.com>
2022-07-27 20:38:49 +0700

    Added accurate argument to roll command For those who want to see the ful result

--- src/cmd/roll.js
+++ src/cmd/roll.js
@@ -3,11 +3,16 @@ const {MessageEmbed} = require('discord.js');
 
 module.exports = new Command({
     name: "roll",
+    alias: ["random"],
     description: "roll a random number from 1 to 100",
 
     async run(message, args, client) {
         if (message.author.bot) return;
-        var n = args.slice(1).join(" ")
+        var temp = args.slice(1).join(" ")
+        var acc = args.slice(2).join(" ");
+        var l = temp.length;
+        var l2 = acc.length
+        var n = temp.substr(0, l-l2)
         var ch = 1
         //console.log(message.author.tag,"used a!roll",n)
         if (!isNaN(n)){
@@ -16,8 +21,13 @@ module.exports = new Command({
                 var ch = 0
             } else
             if (n < 1) {message.reply('You entered an invalid number!')} else {var ch = 0}
-        var numba = Math.floor(Math.random() * n)+1;
+        var numba = (Math.random() * n)+1;
     var name = message.author.username;
+    if (acc = "accurate") {
+            var numble = numba
+        } else {
+            var numble = Math.floor(numba)
+        }
     var time = new Date().toLocaleTimeString('en-US', { hour12: false,
                                              hour: "numeric",
                                              minute: "numeric",
@@ -25,7 +35,7 @@ module.exports = new Command({
     console.log("[",time,"]",message.author.tag,"rolled a random number");
     const embed = new MessageEmbed()
     .setColor(`#2EFFEE`)
-    .setTitle(`${name} rolled ${numba}`)
+    .setTitle(`${name} rolled ${numble}`)
     .setTimestamp()
     //.setFooter(`Rolled by ${name}`)
 
