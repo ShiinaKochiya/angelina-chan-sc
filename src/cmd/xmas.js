6cbcfdd9692c587564750c738767c29a05a40b49
Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>
2022-12-14 22:40:14 +0700

    xmas revamped, also time it to +07
    Signed-off-by: Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>

--- src/cmd/xmas.js
+++ src/cmd/xmas.js
@@ -4,12 +4,12 @@ module.exports = new Command({
     name: "christmas",
     description: "christmas countdown",
     alias:["xmas"],
-
+    
     async run(message, args, client) {
         const now = new Date();
         var date_now = now.valueOf()
-        var date_future = 1671948000000
-        
+        var date_future = 1671901200000
+        if (date_now < date_future){
         var delta = Math.abs(date_future - date_now) / 1000;
         var days = Math.floor(delta / 86400);
         delta -= days * 86400;
@@ -17,6 +17,15 @@ module.exports = new Command({
         var hours = Math.floor(delta / 3600) % 24;
         delta -= hours * 3600;
         
-        message.reply(`There's ${days} days and ${hours} hours left until Christmas`)
+        message.reply(`There's ${days} days and ${hours} hours left until Christmas`);}
+	else if (date_now == date_future && date_now < 1671987600000) {message.reply(`Happy Christmas!`);}
+	else {message.reply(`Christmas's done already, go wait for new year's`);}
+	var time = new Date().toLocaleTimeString('en-US', { hour12: false,
+                                                 hour: "numeric",
+                                                 minute: "numeric",
+                                                 second: "numeric"})
+        console.log("[",time,"]", message.author.tag,`wanted to know when's christmas`);
+	
+
     }   
 });
