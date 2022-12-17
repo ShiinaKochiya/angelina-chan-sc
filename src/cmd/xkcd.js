2e40c17fa34b0ca1be2afca7683ef09c0a5ebf46
Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>
2022-12-17 22:02:12 +0700

    xkcd uses axios now, node 18 requirement removed
    Signed-off-by: Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>

--- src/cmd/xkcd.js
+++ src/cmd/xkcd.js
@@ -2,7 +2,7 @@
 
 const Command = require("../structures/Command");
 const {MessageEmbed} = require('discord.js');
-
+const axios = require(`axios`);
 
 module.exports = new Command({
     name: "xkcd",
@@ -10,40 +10,36 @@ module.exports = new Command({
 
     async run(message, args, client){
         if (message.author.bot) return;
-        const latestraw=await fetch('https://xkcd.com/info.0.json');
-        const latest=await latestraw.json();
-        //console.log(latest);
+        const latest =  await axios.get(`https://xkcd.com/info.0.json`);
         var type=args.slice(1).join(" "); 
         if (type === '' || type === 'latest') {
             const embed = new MessageEmbed()
             .setColor(`RANDOM`)
-            .setTitle(`xkcd #${latest.num} - ${latest.safe_title}`)
-            .setImage(latest.img)
-            .setFooter(latest.alt)
+            .setTitle(`xkcd #${latest.data.num} - ${latest.data.safe_title}`)
+            .setImage(latest.data.img)
+            .setFooter(latest.data.alt)
             .setURL(`https://xkcd.com/`)
             message.reply({embeds: [embed]})
         }
         else if (type === 'random') {
-            var rng=Math.floor(Math.random() * latest.num);
-            const randomraw=await fetch(`https://xkcd.com/${rng}/info.0.json`);
-            const random=await randomraw.json();
+            var rng=Math.floor(Math.random() * latest.data.num);
+            const random =  await axios.get(`https://xkcd.com/${rng}/info.0.json`);
             const embed = new MessageEmbed()
             .setColor(`RANDOM`)
-            .setTitle(`xkcd #${random.num} - ${random.safe_title}`)
-            .setImage(random.img)
-            .setFooter(random.alt)
-            .setURL(`https://xkcd.com/${random.num}`)
+            .setTitle(`xkcd #${random.data.num} - ${random.data.safe_title}`)
+            .setImage(random.data.img)
+            .setFooter(random.data.alt)
+            .setURL(`https://xkcd.com/${random.data.num}`)
             message.reply({embeds: [embed]})
         }
-        else if (type <= latest.num) {
-            const preciseraw=await fetch (`https://xkcd.com/${type}/info.0.json`);
-            const precise=await preciseraw.json();
+        else if (type <= latest.data.num) {
+            const precise=await axios.get (`https://xkcd.com/${type}/info.0.json`);
             const embed = new MessageEmbed()
             .setColor(`RANDOM`)
-            .setTitle(`xkcd #${precise.num} - ${precise.safe_title}`)
-            .setImage(precise.img)
-            .setFooter(precise.alt)
-            .setURL(`https://xkcd.com/${precise.num}`)
+            .setTitle(`xkcd #${precise.data.num} - ${precise.data.safe_title}`)
+            .setImage(precise.data.img)
+            .setFooter(precise.data.alt)
+            .setURL(`https://xkcd.com/${precise.data.num}`)
             message.reply({embeds: [embed]})
         }
         else message.reply(`Please check your input. Options are latest, random or specific number.`);
