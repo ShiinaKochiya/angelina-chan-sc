122a9fb0206ff71aba3c4dd0211604add3d5df99
Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>
2022-12-17 15:01:04 +0700

    Add xkcd link to the xkcd command embed
    Signed-off-by: Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>

--- src/cmd/xkcd.js
+++ src/cmd/xkcd.js
@@ -20,6 +20,7 @@ module.exports = new Command({
             .setTitle(`xkcd #${latest.num} - ${latest.safe_title}`)
             .setImage(latest.img)
             .setFooter(latest.alt)
+            .setURL(`https://xkcd.com/`)
             message.reply({embeds: [embed]})
         }
         else if (type === 'random') {
@@ -31,6 +32,7 @@ module.exports = new Command({
             .setTitle(`xkcd #${random.num} - ${random.safe_title}`)
             .setImage(random.img)
             .setFooter(random.alt)
+            .setURL(`https://xkcd.com/${random.num}`)
             message.reply({embeds: [embed]})
         }
         else if (type <= latest.num) {
@@ -41,6 +43,7 @@ module.exports = new Command({
             .setTitle(`xkcd #${precise.num} - ${precise.safe_title}`)
             .setImage(precise.img)
             .setFooter(precise.alt)
+            .setURL(`https://xkcd.com/${precise.num}`)
             message.reply({embeds: [embed]})
         }
         else message.reply(`Please check your input. Options are latest, random or specific number.`);
