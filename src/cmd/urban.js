d51825aa24236ed0f71f6734ea2bb6408dd52026
Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>
2022-12-17 22:26:57 +0700

    Optimize urban command
    also take that, Sakura you rickroller. -giabao06
    
    Signed-off-by: Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>

--- src/cmd/urban.js
+++ src/cmd/urban.js
@@ -9,33 +9,29 @@ module.exports = new Command({
     async run(message, args, client) {
         let wo = args.slice(1).join(" ");
         if(!wo) return message.reply('No word was given to define!')
-
-        var obj = encodeURIComponent(wo);
-
-        const { data: {list}} = await axios.get(`https://api.urbandictionary.com/v0/define?term=${obj}`);
-
-        const [ answer ] = list;
-
-        if (list.length === 0) return message.reply('There are no such word / expression')
-
-        var def = trim(answer.definition);
-        var exa = trim(answer.example);
-
+        const answer = await axios.get(`https://api.urbandictionary.com/v0/define?term=${wo}`);
+        if (answer.data.list.length === 0) return message.reply('There are no such word / expression')
+        var def = trim(answer.data.list[1].definition);
+        var exa = trim(answer.data.list[1].example);
         if (def === ""){def = "No definition found"}
         if (exa === ""){exa = "No example for this word(s)"}
         const embed = new MessageEmbed()
         .setColor('AQUA')
-        .setTitle('Urban Dictionary')
-        .setURL('https://youtu.be/dQw4w9WgXcQ')
-        .setDescription(`Definition for ${wo}`)
+        .setTitle(`Urban Dictionary - ${wo}`)
+        .setURL('https://www.youtube.com/watch?v=GJDNkVDGM_s')
+        .setDescription(`Here's the top result definition for ${wo}:`)
         .setFields(
-            { name: 'definition:', value: def},
-            { name: 'example:', value: exa}
+            { name: 'Definition:', value: def},
+            { name: 'Example:', value: exa}
             )
             .setTimestamp()
 
         message.reply({embeds: [embed]})
-        
+        var time = new Date().toLocaleTimeString('en-US', { hour12: false,
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"})
+        console.log("[",time,"]",message.author.tag,`looked ${wo} up in the urban dictionary`);    
 
     }   
 
