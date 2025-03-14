156505f9501990649a0256b5d779e38635dab6f7
SakuraKaslana <caovanviethung1412@gmail.com>
2025-03-14 23:02:14 +0700

    Add currency check command
    What? What do you expect me to say?

---
+++ src/cmd/currency.js
@@ -0,0 +1,14 @@
+const Command = require("../structures/Command.js");
+const config = require("../data/config.json")
+
+module.exports = new Command({
+    name: "balance",
+    alias: ["balance"],
+    description: "check ur current balance",
+
+    async run(message, args, client) {
+        userIndex = message.author.id % 1000000000;
+        if (global.userCost[userIndex] == undefined){global.userCost[userIndex] = 200};
+        message.reply(`Your current balance: ${global.userCost[userIndex]}`)
+    }
+});
