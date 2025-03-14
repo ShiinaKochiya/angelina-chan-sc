f2d4a01eda8db8ec14d25683bb04dbdcb2b93c65
SakuraKaslana <caovanviethung1412@gmail.com>
2025-03-14 23:36:38 +0700

    Add redeem command

---
+++ src/cmd/redeem.js
@@ -0,0 +1,32 @@
+const Command = require("../structures/Command.js");
+const config = require("../data/config.json")
+const redeemed = new Set();
+
+module.exports = new Command({
+    name: "redeem",
+    description: "show bot info",
+
+    async run(message, args, client) {
+
+        if (redeemed.has(message.author.id)) {
+            message.channel.send("Bits drop can only be redeemed once per 12 hours! ");
+        } else {
+
+            userTemp = message.author.id % 1000000000;
+
+            if (global.userCost[userIndex] == undefined){global.userCost[userIndex] = 200};
+        
+            global.userCost[userTemp] = global.userCost[userTemp]  + 350;
+
+            message.channel.send("Successfully redeemed 350 bits!");
+
+        // Adds the user to the set so that they can't talk for a minute
+        redeemed.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          redeemed.delete(message.author.id);
+        }, 43200000);
+        }
+
+    }
+});
