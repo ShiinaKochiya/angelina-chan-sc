51b88cf6be64fb17ec2c50bcda7b5b356666cf44
SakuraKaslana <caovanviethung1412@gmail.com>
2025-12-22 12:19:27 +0700

    chore: market part 19

---
+++ src/cmd/anxin.js
@@ -0,0 +1,22 @@
+const Command = require("../structures/Command.js");
+const config = require("../data/config.json")
+const { getMoney, updateMoneyCache } = require("../moneySchema.js");
+
+module.exports = new Command({
+    name: "anxin",
+    alias: ["50k"],
+    description: "xin 50k",
+
+    async run(message, args, client) {
+        const userId = message.author.id;
+        const money = await getMoney(userId);
+        const wallet = typeof money.wallet === 'bigint' ? money.wallet : BigInt(money.wallet || 0);
+
+        const bonus = BigInt(500000);
+        const newWallet = wallet + bonus;
+
+        updateMoneyCache(userId, { wallet: newWallet });
+
+        message.reply(`Đại đế Kamito thấy bạn thật đáng thương, nên Đại đế đã drop cho bạn ${bonus}\nHiện tại bạn có: ${newWallet.toLocaleString('en-US')} VND`);
+    }
+});
